498ad47b2db1d40357798fcd7bacfcac40825920
Víctor Sales <victor.sales83gmail.com>
2025-07-25 23:08:47 +0200

    fixing routes on dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EventDetail from "./pages/EventDetail";
 import KPIDetail from "./pages/KPIDetail";
 
-const Router =
-  import.meta.env.MODE === "production" ? HashRouter : BrowserRouter;
-
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/event/:id" element={<EventDetail />} />
         <Route path="/event/:id/kpi/:kpi" element={<KPIDetail />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
